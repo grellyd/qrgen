cf452830b44525482b937ca8caa5fa74ff4bee2d
grellyd <graham.brown137@gmail.com>
2019-02-27 18:45:22 -0800

    Generating codes in terminal

--- qrlib/qrlib.go
+++ qrlib/qrlib.go
@@ -1,12 +1,26 @@
 package qrlib
 
 /*
+#include <stdio.h>
 #include <stdlib.h>
 #include <qrcodegen.h>
+
+static void printQr(const uint8_t qrcode[]) {
+	int size = qrcodegen_getSize(qrcode);
+	int border = 4;
+	for (int y = -border; y < size + border; y++) {
+		for (int x = -border; x < size + border; x++) {
+			fputs((qrcodegen_getModule(qrcode, x, y) ? "##" : "  "), stdout);
+		}
+		fputs("\n", stdout);
+	}
+	fputs("\n", stdout);
+}
 */
 import "C"
 
 import (
+	"image"
 	"fmt"
 	"strconv"
 	"unsafe"
@@ -48,8 +62,20 @@ func GenerateCli(args []string) error {
 	} else {
 		return fmt.Errorf("argument error")
 	}
+	i, err := Generate(l, ec)
+	if err!= nil {
+		return fmt.Errorf("unable to generate error: %s", err.Error())
+	}
+	err = write(i)
+	if err != nil {
+		return fmt.Errorf("unable to write image: %s", err.Error())
+	}
+	return nil
+}
 
-	return Generate(l, ec)
+// write the resulting image to disk
+func write(i *image.Image) error{
+	return nil
 }
 
 /*
@@ -57,17 +83,44 @@ bool qrcodegen_encodeText(const char *text, uint8_t tempBuffer[], uint8_t qrcode
 */
 
 // Generate the QR Code
-func Generate(l string, ec int) error {
+func Generate(l string, ec int) (i *image.Image, err error) {
 	clink := C.CString(l)
 	defer C.free(unsafe.Pointer(&clink))
 	b := make([]C.uchar, C.qrcodegen_BUFFER_LEN_MAX)
 	defer C.free(unsafe.Pointer(&b))
 	qr := make([]C.uchar, C.qrcodegen_BUFFER_LEN_MAX)
 	defer C.free(unsafe.Pointer(&qr))
-	ok := C.qrcodegen_encodeText(clink, &b[0], &qr[0], C.enum_qrcodegen_Ecc(1), C.int(1), C.int(40), C.enum_qrcodegen_Mask(-1), false)
+	ok := C.qrcodegen_encodeText(clink, &b[0], &qr[0], C.enum_qrcodegen_Ecc(1), C.int(1), C.int(40), C.enum_qrcodegen_Mask(-1), true)
 	if !ok {
-		return fmt.Errorf("Unable to encodeText")
+		return nil, fmt.Errorf("Unable to encodeText")
 	}
+	size := C.qrcodegen_getSize(&qr[0])
+	fmt.Printf("%v\n", size)
+	fmt.Printf("%v\n", qr[:size*4])
 	fmt.Printf("%v", qr)
-	return nil
+	i, err = buildImage(qr)
+	if err != nil {
+		return nil, fmt.Errorf("unable to build an output image: %s", err.Error())
+	}
+	return i, nil
 }
+
+func buildImage(qr []C.uchar) (i *image.Image, err error) {
+	C.printQr(&qr[0])
+	return nil, nil
+}
+
+/*
+// Prints the given QR Code to the console.
+static void printQr(const uint8_t qrcode[]) {
+	int size = qrcodegen_getSize(qrcode);
+	int border = 4;
+	for (int y = -border; y < size + border; y++) {
+		for (int x = -border; x < size + border; x++) {
+			fputs((qrcodegen_getModule(qrcode, x, y) ? "##" : "  "), stdout);
+		}
+		fputs("\n", stdout);
+	}
+	fputs("\n", stdout);
+}
+*/
