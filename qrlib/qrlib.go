bde94e0bf6c84a4c027d1547fbbd3ba503f4e316
grellyd <graham.brown137@gmail.com>
2019-02-27 16:32:27 -0800

    WIP: cgo qrcodegen bindings. resolved unexpected, still compile errors.

--- qrlib/qrlib.go
+++ qrlib/qrlib.go
@@ -1,5 +1,11 @@
 package qrlib
 
+/*
+#include <stdlib.h>
+#include <qrcodegen.h>
+*/
+import "C"
+
 import (
 	"fmt"
 	"strconv"
@@ -11,6 +17,8 @@ Level M -> 15%
 Level Q -> 25%
 Level H -> 30%
 */
+
+
 
 
 // GenerateCli is a CLI wrapper for Generate
@@ -28,7 +36,7 @@ func GenerateCli(args []string) error {
 			return fmt.Errorf("no link given")
 		}
 		if args[1] != "" {
-			ec, err := strconv.ParseInt(args[1], 10, 0)
+			ec, err := strconv.Atoi(args[1])
 			if err != nil {
 				return fmt.Errorf("error parsing error level: %s", err.Error())
 			}
@@ -43,7 +51,33 @@ func GenerateCli(args []string) error {
 	return Generate(l, ec)
 }
 
+/*
+bool qrcodegen_encodeText(const char *text, uint8_t tempBuffer[], uint8_t qrcode[], enum qrcodegen_Ecc ecl, int minVersion, int maxVersion, enum qrcodegen_Mask mask, bool boostEcl);
+*/
+
 // Generate the QR Code
 func Generate(l string, ec int) error {
+	clink := C.CString(l)
+	defer C.free(clink)
+	b := make([]uint, C.qrcodegen_BUFFER_LEN_MAX)
+	defer C.free(b)
+	qr := make([]uint, C.qrcodegen_BUFFER_LEN_MAX)
+	defer C.free(qr)
+	ecl := C.enum_qrcodegen_Ecc{qrcodegen_Ecc_MEDIUM}
+	defer C.free(ecl)
+	mask := C.enum_qrcodegen_Mask{qrcodegen_Mask_AUTO}
+	defer C.free(mask)
+	ok := C.qrcodegen_encodeText(clink, &b[0], &qr[0], ecl, C.int(1), C.int(40), mask, boost)
+	if !ok {
+		return fmt.Errorf("Unable to encodeText")
+	}
 	return nil
 }
+
+
+
+/*
+uint8_t C.uint(x)
+size_t C.sizeof_x || uint16_t
+enum qrcodegen_Ecc  -> enum_qrcodegen_Ecc
+*/
