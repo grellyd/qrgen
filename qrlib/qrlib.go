d916049fc52d240384ff6899338ef337d80f0053
grellyd <graham.brown137@gmail.com>
2019-03-01 14:45:26 -0800

    WIP cleanup

--- qrlib/qrlib.go
+++ qrlib/qrlib.go
@@ -28,21 +28,37 @@ import (
 	"strconv"
 //	"unsafe"
 )
+
 /*
-Error Codes:
-Level L -> 7% Error Correction Capability
-Level M -> 15%
-Level Q -> 25%
-Level H -> 30%
+ErrorCorrectionLevel Codes:
+Level Low     -> 7% Error Correction Capability
+Level Medium  -> 15%
+Level Quality -> 25%
+Level High    -> 30%
 */
+type ErrorCorrectionLevel int
 
+const (
+	//ECLL Low
+	ECLL ErrorCorrectionLevel = iota
+	//ECLM Medium
+	ECLM 
+	//ECLQ Quality
+	ECLQ
+	//ECLH High
+	ECLH
+)
 
-
+const (
+	// BORDER size around the QR code in pixels
+	BORDER = 2
+)
 
 // GenerateCli is a CLI wrapper for Generate
 func GenerateCli(args []string) error {
 	l := ""
-	ec := 1
+	ec := ECLL
+	f := ""
 
 	for _, s := range args {
 		fmt.Println(s)
@@ -77,22 +93,25 @@ func GenerateCli(args []string) error {
 }
 
 // write the resulting image to disk
-func write(i *image.Image) error{
+func write(i *image.Gray) error {
+	f, _ := os.Create("out.png")
+	defer f.Close()
+	png.Encode(f, i)
 	return nil
 }
 
-/*
-bool qrcodegen_encodeText(const char *text, uint8_t tempBuffer[], uint8_t qrcode[], enum qrcodegen_Ecc ecl, int minVersion, int maxVersion, enum qrcodegen_Mask mask, bool boostEcl);
-*/
-
 // Generate the QR Code
-func Generate(l string, ec int) (i *image.Image, err error) {
+func Generate(l string, ec ErrorCorrectionLevel) (i *image.Gray, err error) {
 	clink := C.CString(l)
 	//defer C.free(unsafe.Pointer(&clink))
 	b := make([]C.uchar, C.qrcodegen_BUFFER_LEN_MAX)
 	//defer C.free(unsafe.Pointer(&b))
 	qr := make([]C.uchar, C.qrcodegen_BUFFER_LEN_MAX)
 	//defer C.free(unsafe.Pointer(&qr))
+	/*
+	From qrcodegen.h
+	bool qrcodegen_encodeText(const char *text, uint8_t tempBuffer[], uint8_t qrcode[], enum qrcodegen_Ecc ecl, int minVersion, int maxVersion, enum qrcodegen_Mask mask, bool boostEcl);
+	*/
 	ok := C.qrcodegen_encodeText(clink, &b[0], &qr[0], C.enum_qrcodegen_Ecc(1), C.int(1), C.int(40), C.enum_qrcodegen_Mask(-1), true)
 	if !ok {
 		return nil, fmt.Errorf("Unable to encodeText")
@@ -108,15 +127,13 @@ func Generate(l string, ec int) (i *image.Image, err error) {
 	return i, nil
 }
 
-func buildImage(qr []C.uchar) (i *image.Image, err error) {
-	border := 2
+func buildImage(qr []C.uchar) (i *image.Gray, err error) {
 	C.printQr(&qr[0])
-	size := int(C.qrcodegen_getSize(&qr[0])) + 2 * border
+	size := int(C.qrcodegen_getSize(&qr[0])) + 2 * BORDER
 	g := image.NewGray(image.Rect(0, 0, size, size))
 	for y := 0; y < size; y++ {
 		for x := 0; x < size; x++ {
-			mod := C.qrcodegen_getModule(&qr[0], C.int(x - border), C.int(y - border))
-			fmt.Printf("x, y: %d, %d --> %v\n", x, y, mod)
+			mod := C.qrcodegen_getModule(&qr[0], C.int(x - BORDER), C.int(y - BORDER))
 			if mod { 
 				g.Set(x, y, color.Black)
 			} else {
@@ -124,23 +141,5 @@ func buildImage(qr []C.uchar) (i *image.Image, err error) {
 			}
 		}
 	}
-	f, _ := os.Create("out.png")
-	defer f.Close()
-	png.Encode(f, g)
-	return nil, nil
+	return g, nil
 }
-
-/*
-// Prints the given QR Code to the console.
-static void printQr(const uint8_t qrcode[]) {
-	int size = qrcodegen_getSize(qrcode);
-	int border = 4;
-	for (int y = -border; y < size + border; y++) {
-		for (int x = -border; x < size + border; x++) {
-			fputs((qrcodegen_getModule(qrcode, x, y) ? "##" : "  "), stdout);
-		}
-		fputs("\n", stdout);
-	}
-	fputs("\n", stdout);
-}
-*/
